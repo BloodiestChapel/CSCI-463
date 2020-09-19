82f97b9e1e9a534fd95ec48f4b1303965e79dcc8
Dillon Chappell <55209913+BloodiestChapel@users.noreply.github.com>
2020-09-18 21:21:10 -0500

    Create quizQs.cpp

---
+++ quizQs.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+int main()
+{
+for (int i = 0; i < 32; i++)
+   {
+        cout  << (x & 0x80000000 ? '1':'0');
+        x = x << 1;
+        if(i % 4 == 0)
+        {
+             cout << " ";
+        }
+    }
+}
